40b6a524243c0278a9635d762e0dabef934ec884
Grace Colette <ilovegrace09@gmail.com>
2019-06-24 07:24:36 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,21 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(elements)
+
+  while elements.length > 0
+  yield
+    element
+  end
+# end
+#   my_each(element) {|element| puts element}
+  
+  
 end
+#yielding_with_arguments(2) {|i| puts i * 2}
+
+# def hello_t(array)
+#   i = 0
+ 
+#   while i < array.length
+#     yield array[i]
+#     i = i + 1
+#   end
+# end
